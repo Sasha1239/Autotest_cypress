8dbe7f1bab9cd8d73c16e498b4fa750b350508cc
Sasha1239 <ham1239@gmail.com>
2021-02-12 17:09:11 +0300

    Шаблон

--- TestCY.js
+++ TestCY.js
@@ -1,5 +1,7 @@
 describe('Try test', function () {
     it('Open start page', function () {
-
+            cy.visit('https://skdf-s-qa.ckr-sota.ru')
+            cy.xpath('//span[text()="Вход для специалисто"]').click() 
+           
     })
 })
