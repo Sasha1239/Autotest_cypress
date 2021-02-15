b34d1348bd8307dcffff81a23b77a513902124df
Sasha1239 <ham1239@gmail.com>
2021-02-15 11:51:48 +0300

    Шаблон

--- TestCY.js
+++ TestCY.js
@@ -1,7 +1,7 @@
 describe('Try test', function () {
     it('Open start page', function () {
             cy.visit('https://тест.скдф.рф/')
-            cy.get('//span[text()="Вход для специалистов"]').click() 
+            cy.xpath('//span[text()="Вход для специалистов"]').click() 
            
     })
 })
