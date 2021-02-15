a7726f8647a2f3d3f7736c11985cc9a0adb11849
Sasha1239 <ham1239@gmail.com>
2021-02-15 17:50:37 +0300

    Выбор значения из дроп дауна "Международный маршрут" без применения

--- TestCY.js
+++ TestCY.js
@@ -11,7 +11,6 @@ describe('Try test', function () {
             cy.xpath('//div//span[@class="krkn__link krkn__nested-dropdown__option-text" and text()="Дороги"]').click()
             cy.xpath('//div/a[@href="/roads/32197941"]').click()
             cy.xpath('//div/span[text()="Редактировать"]').click()
-            cy.xpath('//h2[text()="Владелец дороги"]').click()
             cy.xpath('//div[@class="krkn__multi-dropdown__placeholder-text" and text()="Международный маршрут"]').click()
             cy.xpath ('//span[@class="krkn__multi-dropdown__option-text"]/span[text()="AH30"]').click({force: true})   
             
