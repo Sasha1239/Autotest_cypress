6b7193140b93a58761d3d098db15b3e009b64ff3
Sasha1239 <ham1239@gmail.com>
2021-02-16 16:01:45 +0300

    Раскрытие панел фильтров

--- Панель фильтров.js
+++ Панель фильтров.js
@@ -7,6 +7,11 @@ describe('Try test', function () {
             cy.xpath('//input[@id="Password"]').click()
                 .type('SOTA2101').should('have.value', 'SOTA2101')
             cy.xpath('//button[text()="Войти"]').click()
-
+            cy.xpath('//div[text()="Цифровая база"]').click()
+            cy.xpath('//div//span[@class="krkn__link krkn__nested-dropdown__option-text" and text()="Дороги"]').click()
+            cy.wait(1000)
+            cy.xpath('//span[text()="Фильтры"]').click()
+            
+            
     })
 })            
