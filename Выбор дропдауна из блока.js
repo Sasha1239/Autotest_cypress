f76657cbd03ec5ab7054f64fc72647f2a50b92d4
Sasha1239 <ham1239@gmail.com>
2021-02-16 10:00:34 +0300

    Тест

--- Выбор дропдауна из блока.js
+++ Выбор дропдауна из блока.js
@@ -11,8 +11,8 @@ describe('Try test', function () {
             cy.xpath('//div//span[@class="krkn__link krkn__nested-dropdown__option-text" and text()="Дороги"]').click()
             cy.xpath('//div/a[@href="/roads/32197941"]').click()
             cy.xpath('//div/span[text()="Редактировать"]').click()
-            cy.xpath('//div[@class="krkn__multi-dropdown__placeholder-text" and text()="Международный маршрут"]').click()
-            cy.xpath('//span[@class="krkn__multi-dropdown__option-text"]/span[text()="AH30"]').click({force: true})   
+            cy.xpath('//h2[text()="Наименование блока"]').click()
+            cy.xpath('//div[@class="krkn__multi-dropdown__placeholder" and text()="Владелец"]').click()
             
             
            
