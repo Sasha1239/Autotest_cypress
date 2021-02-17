49b9326820a25161060d62b81842457a1abdcab9
sherbakov <sherbakov@ckr-sota.ru>
2021-02-17 10:18:08 +0300

    Тест

--- Проверка использования тестовых данных.js
+++ Проверка использования тестовых данных.js
@@ -1,5 +1,13 @@
 describe('Try test', function () {
     it('Open start page', function () {
         cy.fixture('Вход в СКДФ, ТЕСТ.js')
+        cy.xpath('//div[text()="Цифровая база"]').click()
+        cy.xpath('//div//span[@class="krkn__link krkn__nested-dropdown__option-text" and text()="Дороги"]').click()
+        cy.xpath('//div/a[@href="/roads/32197941"]').click()
+        cy.xpath('//div/span[text()="Редактировать"]').click()
+        cy.xpath('//div[@class="krkn__multi-dropdown__placeholder-text" and text()="Международный маршрут"]').click()
+        cy.xpath('//span[@class="krkn__multi-dropdown__option-text"]/span[text()="AH30"]').click({force: true})   
+        cy.xpath('//button[@class="ui button krkn__btn" and text()="Применить (1)"]').click({force: true})
+        cy.xpath('//button[text()="Сохранить"]').click()
     })
 })    
